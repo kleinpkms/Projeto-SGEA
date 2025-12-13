0d0a420c2dbb95ce52ddb720d88003231272c4bb
Marcelo_rba <marcelo.rbangelini@sempreceub.com>
2025-12-13 00:45:46 -0300

    Update apps.py

--- core/apps.py
+++ core/apps.py
@@ -1,8 +1,13 @@
 from django.apps import AppConfig
 
-class CoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
 
-    def ready(self):
-        import core.signals
+class CoreConfig(AppConfig):
+	default_auto_field = 'django.db.models.BigAutoField'
+	name = 'core'
+
+	def ready(self):
+		# import signal handlers to ensure they're registered
+		try:
+			from . import signals  # noqa: F401
+		except Exception:
+			pass
